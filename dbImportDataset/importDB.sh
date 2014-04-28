030b7b8fe93b92352a3731fbc631cdcf7ac5b89c
HeberLZ <heberlopez@gmail.com>
2014-04-28 01:59:41 -0300

    fix log y dependencias

--- dbImportDataset/importDB.sh
+++
@@ -1,3 +0,0 @@
-mongoimport --db gdg-db --collection events --jsonArray events.json
-mongoimport --db gdg-db --collection posts --jsonArray posts.json
-mongoimport --db gdg-db --collection users --jsonArray users.json
